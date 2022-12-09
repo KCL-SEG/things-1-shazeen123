bbed48e30fac503fb508167f46e9e65db71594fd
shazeen123 <shazeenshaheen2003@gmail.com>
2022-12-09 14:58:15 +0000

    test

--- thingproject/urls.py
+++ thingproject/urls.py
@@ -15,10 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-
 from things import views
 
 urlpatterns = [
-    path('', views.thing, name="home"),
     path('admin/', admin.site.urls),
+    path('', views.thing, name='home')
 ]
